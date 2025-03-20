66e94ae9a0a2bdbe07fd1c0e882b44a279f8b025
DanielFz07 <th3d4n13r@gmail.com>
2025-03-20 10:28:59 -0400

    Fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://danielfz07.github.io/',
+    site: 'https://danielfz07.github.io',
     base: '/portafolio'
 });
