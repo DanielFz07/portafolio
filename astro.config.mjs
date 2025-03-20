2f2821bca5bfeba6cbe15390a38d0e767979be42
DanielFz07 <th3d4n13r@gmail.com>
2025-03-20 10:20:36 -0400

    Fix css and favicon issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://danielfz07.github.io/portafolio/',
-    base: 'DanielFz07/portafolio',
+    site: 'https://danielfz07.github.io/',
+    base: '/portafolio'
 });
